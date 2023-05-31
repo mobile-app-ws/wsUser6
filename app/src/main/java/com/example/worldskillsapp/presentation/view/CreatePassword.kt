2cb90a4a75c5741e2094153cd41d821d5e816e92
student <riba4koff@mail.ru>
2023-05-31 19:06:56 +0300

    Path-3

--- app/src/main/java/com/example/worldskillsapp/presentation/view/CreatePassword.kt
+++ app/src/main/java/com/example/worldskillsapp/presentation/view/CreatePassword.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -160,6 +161,7 @@ fun ButtonKeyBoard(
     Card(
         modifier = modifier
             .size(80.dp)
+            .clip(CircleShape)
             .clickable { onClick(title) },
         shape = CircleShape,
         colors = CardDefaults.cardColors(
