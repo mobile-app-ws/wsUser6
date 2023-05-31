32f17a53fee5cf7f3d7f6764fd8990a457f38ae0
student <riba4koff@mail.ru>
2023-05-31 11:35:03 +0300

    Initial commit

--- app/src/main/java/com/example/worldskillsapp/presentation/view/SignIn.kt
+++ app/src/main/java/com/example/worldskillsapp/presentation/view/SignIn.kt
@@ -1,7 +1,14 @@
 package com.example.worldskillsapp.presentation.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination
@@ -12,7 +19,9 @@ fun SignIn(){
 
 @Composable
 fun SignInContent(){
-
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        Text("Sign in", fontSize = 24.sp, color = Color.Green)
+    }
 }
 
 @Preview
